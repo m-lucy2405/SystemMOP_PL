c612fb9e681ad4a3e631de796cdf2cdf0aa8ff1d
SrCroy <rubiocr007@gmail.com>
2025-06-22 16:47:34 -0600

    subiend index en main

--- lineal_solver_PL_MOP/lineal_solver/lineal_solver/settings.py
+++ lineal_solver_PL_MOP/lineal_solver/lineal_solver/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS', '').split(',') if host.strip()]
 
 # Application definition
 INSTALLED_APPS = [
@@ -96,7 +96,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
--- lineal_solver_PL_MOP/lineal_solver/lineal_solver/urls.py
+++ lineal_solver_PL_MOP/lineal_solver/lineal_solver/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from lineal_solver import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('simplex/', include('apps.metodo_simplex.urls')),
     path('grafico/', include('apps.metodo_grafico.urls')),
+    path('', views.index, name='index'),
 ]

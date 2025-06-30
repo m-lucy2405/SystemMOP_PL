d8049ccf44d00fdc6585d4e2eba3f942081ef04d
manuelmv15 <mv23036@ues.edu.sv>
2025-06-30 15:01:05 -0600

    todo perfil

--- lineal_solver_PL_MOP/lineal_solver/lineal_solver/urls.py
+++ lineal_solver_PL_MOP/lineal_solver/lineal_solver/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from lineal_solver import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('simplex/', include('apps.metodo_simplex.urls')),
     path('grafico/', include('apps.metodo_grafico.urls')),
     path('', views.home, name='home'),
-    path('login/', include('apps.usuarios.urls'), name='login'),
-]
+    path('usuario/', include('apps.usuarios.urls'), name='login'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

0b118ae4458e2c5218eac4a7423cfd8436f5c598
m-lucy2405 <167549729+m-lucy2405@users.noreply.github.com>
2025-07-03 01:46:26 -0600

    settings Update

--- lineal_solver_PL_MOP/lineal_solver/core/settings.py
+++ lineal_solver_PL_MOP/lineal_solver/core/settings.py
@@ -7,7 +7,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
